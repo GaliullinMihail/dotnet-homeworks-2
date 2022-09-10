075f2eee2986e1366618c8d470775becb65192e6
Михаил Галиуллин <mihail.117@mail.ru>
2022-09-11 01:51:59 +0300

    add HW1 branch

--- HomeWork1/Hw1/Parser.cs
+++ HomeWork1/Hw1/Parser.cs
@@ -2,18 +2,48 @@ namespace Hw1;
 
 public static class Parser
 {
-    public static void ParseCalcArguments(string[] args, 
-        out double val1, 
-        out CalculatorOperation operation, 
+    public static void ParseCalcArguments(string[] args,
+        out double val1,
+        out CalculatorOperation operation,
         out double val2)
     {
-        throw new NotImplementedException();
+        if (!IsArgLengthSupported(args)) throw new ArgumentException();
+        try
+        {
+            val1 = double.Parse(args[0]);
+            val2 = double.Parse(args[2]);
+        }
+
+        catch
+        {
+            throw new ArgumentException();
+        }
+
+        operation = ParseOperation(args[1]);
+        if (operation == CalculatorOperation.Undefined)
+            throw new ArgumentOutOfRangeException();
+
+
     }
 
     private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
 
     private static CalculatorOperation ParseOperation(string arg)
     {
-        throw new NotImplementedException();
+        switch (arg)
+        {
+            case "+":
+                return CalculatorOperation.Plus;
+            case "-":
+                return CalculatorOperation.Minus; ;
+            case "*":
+                return CalculatorOperation.Multiply;
+            case "/":
+                return CalculatorOperation.Divide;
+            case "_":
+                return CalculatorOperation.Undefined;
+            default:
+                throw new InvalidOperationException();
+        }
     }
 }
