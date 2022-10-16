2d2e3048cee997c7e55d4bb414f8fbae97561fdf
Михаил Галиуллин <mihail.117@mail.ru>
2022-10-16 18:37:12 +0300

    add hw5

--- Homework5/Hw5.Tests/CalculatorTests.fs
+++ Homework5/Hw5.Tests/CalculatorTests.fs
@@ -12,6 +12,10 @@ let epsilon: decimal = 0.001m
 [<InlineData(15, 5, CalculatorOperation.Minus, 10)>]
 [<InlineData(15, 5, CalculatorOperation.Multiply, 75)>]
 [<InlineData(15, 5, CalculatorOperation.Divide, 3)>]
+[<InlineData(20, 4, CalculatorOperation.Plus, 24)>]
+[<InlineData(20, 4, CalculatorOperation.Minus, 16)>]
+[<InlineData(20, 4, CalculatorOperation.Multiply, 80)>]
+[<InlineData(20, 4, CalculatorOperation.Divide, 5)>]
 let ``+, -, *, / work return correct calculation results with ints`` (value1 : int, value2: int, operation, expectedValue : int) =
     //act
     let actual = calculate value1 operation value2
@@ -24,6 +28,10 @@ let ``+, -, *, / work return correct calculation results with ints`` (value1 : i
 [<InlineData(15.6, 5.6, CalculatorOperation.Minus, 10)>]
 [<InlineData(15.6, 5.6, CalculatorOperation.Multiply, 87.36)>]
 [<InlineData(15.6, 5.6, CalculatorOperation.Divide, 2.7857)>]
+[<InlineData(1.1, 1.1, CalculatorOperation.Plus, 2.2)>]
+[<InlineData(1.1, 1.1, CalculatorOperation.Minus, 0)>]
+[<InlineData(1.1, 1.1, CalculatorOperation.Multiply, 1.21)>]
+[<InlineData(1.1, 1.1, CalculatorOperation.Divide, 1.0)>]
 let ``+, -, *, / work return correct calculation results with floats``
     (value1 : float, value2: float, operation, expectedValue : float) =
     //act
@@ -37,6 +45,10 @@ let ``+, -, *, / work return correct calculation results with floats``
 [<InlineData(15.6, 5.6, CalculatorOperation.Minus, 10)>]
 [<InlineData(15.6, 5.6, CalculatorOperation.Multiply, 87.36)>]
 [<InlineData(15.6, 5.6, CalculatorOperation.Divide, 2.7857)>]
+[<InlineData(1.1, 1.1, CalculatorOperation.Plus, 2.2)>]
+[<InlineData(1.1, 1.1, CalculatorOperation.Minus, 0)>]
+[<InlineData(1.1, 1.1, CalculatorOperation.Multiply, 1.21)>]
+[<InlineData(1.1, 1.1, CalculatorOperation.Divide, 1.0)>]
 let ``+, -, *, / work return correct calculation results with doubles``
     (value1 : double, value2: double, operation, expectedValue : double) =
     //act
@@ -50,6 +62,10 @@ let ``+, -, *, / work return correct calculation results with doubles``
 [<InlineData(15.6, 5.6, CalculatorOperation.Minus, 10)>]
 [<InlineData(15.6, 5.6, CalculatorOperation.Multiply, 87.36)>]
 [<InlineData(15.6, 5.6, CalculatorOperation.Divide, 2.7857)>]
+[<InlineData(1.1, 1.1, CalculatorOperation.Plus, 2.2)>]
+[<InlineData(1.1, 1.1, CalculatorOperation.Minus, 0)>]
+[<InlineData(1.1, 1.1, CalculatorOperation.Multiply, 1.21)>]
+[<InlineData(1.1, 1.1, CalculatorOperation.Divide, 1.0)>]
 let ``+, -, *, / work return correct calculation results with decimals``
     (value1 : decimal, value2: decimal, operation, expectedValue : decimal) =
     //act
--- Homework5/Hw5/Program.fs
+++ Homework5/Hw5/Program.fs
@@ -1,3 +1,18 @@
-﻿open System
+﻿module Hw5.Program
 
-(NotImplementedException() |> raise)       
+open System
+open Hw5
+
+let success (arg1, operation, arg2) =
+    printfn $"{Calculator.calculate arg1 operation arg2}"
+    0
+
+let failure message =
+    printfn $"{message}"
+    -1
+
+[<EntryPoint>]
+let main (args: string[]) =
+    match Parser.parseCalcArguments args with
+    | Ok parsedArgs -> success parsedArgs
+    | Error message -> failure message
