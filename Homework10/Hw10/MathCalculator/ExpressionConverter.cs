6244e8ddc0c03c594ecd3aaf085d279e5ad39887
GaliullinMihail <55558861+GaliullinMihail@users.noreply.github.com>
2022-11-13 19:06:35 +0300

    Update ExpressionConverter.cs

--- Homework10/Hw10/MathCalculator/ExpressionConverter.cs
+++ Homework10/Hw10/MathCalculator/ExpressionConverter.cs
@@ -7,8 +7,8 @@ public static class ExpressionConverter
     public static async Task<double> VisitAsync(Expression expression)
     {
         var visitor = new MyVisitor();
-        var b = visitor.VisitWith(expression);
-        return await b[expression].Value;
+        var result = visitor.VisitWith(expression);
+        return await result[expression].Value;
     }
     
 }
