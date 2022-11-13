38576b35642800224a8ce87ce019b2fd6260998f
GaliullinMihail <55558861+GaliullinMihail@users.noreply.github.com>
2022-11-13 19:00:52 +0300

    add MathCalculatorService

--- Homework10/Hw10/Services/MathCalculator/MathCalculatorService.cs
+++ Homework10/Hw10/Services/MathCalculator/MathCalculatorService.cs
@@ -1,4 +1,7 @@
+using System.Collections.ObjectModel;
+using System.Linq.Expressions;
 using Hw10.Dto;
+using Hw10.MathCalculator;
 
 namespace Hw10.Services.MathCalculator;
 
@@ -6,6 +9,16 @@ public class MathCalculatorService : IMathCalculatorService
 {
     public async Task<CalculationMathExpressionResultDto> CalculateMathExpressionAsync(string? expression)
     {
-        throw new NotImplementedException();
+        try
+        {
+            
+            var b = Parser.Parser.Parse(expression);
+            var c =  await ExpressionConverter.VisitAsync(b);
+            return new CalculationMathExpressionResultDto(c);
+        }
+        catch (Exception e)
+        {
+            return new CalculationMathExpressionResultDto(e.Message);
+        }
     }
 }
