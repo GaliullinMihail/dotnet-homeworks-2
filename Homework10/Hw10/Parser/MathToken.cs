f65a0689fd52ea9dda4fd06c7f94bb600f48e73f
GaliullinMihail <55558861+GaliullinMihail@users.noreply.github.com>
2022-11-13 18:58:24 +0300

    add MathToken

--- Homework10/Hw10/Parser/MathToken.cs
+++ Homework10/Hw10/Parser/MathToken.cs
@@ -1,6 +1,34 @@
-﻿namespace Hw10.Parser;
+using static Hw10.Parser.MathTokenType;
+namespace Hw10.Parser;
 
 public class MathToken
 {
+    public readonly MathTokenType Type;
     
+    public string Value;
+
+    public MathToken(MathTokenType type, string value)
+    {
+        Type = type;
+        Value = value;
+    }
+
+    public bool IsBinaryOperator() =>
+        Type switch
+        {
+            Plus => true,
+            Minus => true,
+            Multiply => true,
+            Divide => true,
+            _ => false
+        };
+
+    public bool IsUnaryOperator() => Type == Negate;
+
+    public bool IsOperator() => IsBinaryOperator() || IsUnaryOperator();
+    
+
+    public bool IsNumber() => Type == Number;
+
+    public int GetPrecedence() => OperatorPrecedence.GetOperatorPrecedence(Type);
 }
