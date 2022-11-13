e8beb65dad433f56cd7321f7ea53f3499f9b64da
GaliullinMihail <55558861+GaliullinMihail@users.noreply.github.com>
2022-11-13 18:59:09 +0300

    Add OperatorPrecedence

--- Homework10/Hw10/Parser/OperatorPrecedence.cs
+++ Homework10/Hw10/Parser/OperatorPrecedence.cs
@@ -1,6 +1,15 @@
-﻿namespace Hw10.Parser;
+using static Hw10.Parser.MathTokenType;
+namespace Hw10.Parser;
 
-public class OperatorPrecedence
+public static class OperatorPrecedence
 {
-    
+    public static int GetOperatorPrecedence(MathTokenType token) =>
+        token switch
+        {
+            OpenBracket or CloseBracket => -1,
+            Plus or Minus => 0,
+            Multiply or Divide => 1,
+            Negate => 2,
+            _ => throw new ArgumentOutOfRangeException()
+        };
 }
