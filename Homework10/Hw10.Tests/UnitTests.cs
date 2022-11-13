516cadeb44fa024569e360991c8857f42e4712e1
GaliullinMihail <55558861+GaliullinMihail@users.noreply.github.com>
2022-11-13 19:02:54 +0300

    add UnitTests

--- Homework10/Hw10.Tests/UnitTests.cs
+++ Homework10/Hw10.Tests/UnitTests.cs
@@ -1,6 +1,36 @@
-﻿namespace Homework10.Tests;
+using System;
+using System.Linq.Expressions;
+using Hw10.MathCalculator;
+using Hw10.Parser;
+using Xunit;
+
+namespace Hw10.Tests;
 
 public class UnitTests
 {
-    
+    [Fact]
+    public void TestTokenizerTokenOperatorOutOfRange()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => Tokenizer.GetTokenOperator("123", 1));
+    }
+
+    [Fact]
+    public void TestTokenizerGetTokenBracketOutOfRange()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => Tokenizer.GetTokenBracket("123", 1));
+    }
+
+    [Fact]
+    public void TestGetOperatorPrecedenceOutOfRange()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+            OperatorPrecedence.GetOperatorPrecedence(MathTokenType.Number));
+    }
+
+    [Fact]
+    public void TestMyVisitorCalculateOutOfRange()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+            new MyVisitor().Calculate(Expression.Decrement(Expression.Constant(1))));
+    }
 }
