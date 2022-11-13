26b89c44874e908ccec4fbb7d223f6310c11069e
GaliullinMihail <55558861+GaliullinMihail@users.noreply.github.com>
2022-11-13 19:21:34 +0300

    update Program(exclude from code coverage)

--- Homework10/Hw10/Program.cs
+++ Homework10/Hw10/Program.cs
@@ -3,34 +3,37 @@ using System.Diagnostics.CodeAnalysis;
 using Hw10.Configuration;
 using Hw10.DbModels;
 using Microsoft.EntityFrameworkCore;
-var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
-
-builder.Services
-    .AddMathCalculator()
-    .AddCachedMathCalculator();
-
-builder.Services.AddDbContext<ApplicationContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-var app = builder.Build();
-if (!app.Environment.IsDevelopment())
+[ExcludeFromCodeCoverage]
+public class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-app.UseAuthorization();
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Calculator}/{action=Index}/{id?}");
-app.Run();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-namespace Hw10
-{
-    [ExcludeFromCodeCoverage]
-    public partial class Program { }
+        builder.Services.AddControllersWithViews();
+
+        builder.Services
+            .AddMathCalculator()
+            .AddCachedMathCalculator();
+
+        builder.Services.AddDbContext<ApplicationContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        var app = builder.Build();
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Calculator}/{action=Index}/{id?}");
+        app.Run();
+    }
 }
